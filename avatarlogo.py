f35ed07621482f36abe0d5a8766dd850374b7f87
Miroslav Hájek <mirkousko@gmail.com>
2017-07-04 20:05:54 +0200

    Basic class structure

--- avatarlogo.py
+++ avatarlogo.py
@@ -1,7 +1,22 @@
+import math
 from PIL import Image, ImageDraw
 
-img = Image.new('RGB', (640, 480))
+class MathLogo:
 
-canvas = ImageDraw.Draw(img)
-canvas.line((0, 0) + img.size, fill=255)
-img.save('avatar.png', 'PNG')
+    def __init__(self, size):
+        self.img = Image.new('RGB', size)
+        self.canvas = ImageDraw.Draw(img)
+
+    def save(self, filename):
+        self.img.save(filename, filename.rpartition('.').upper())
+        
+    def __rose_calc(self, degrees):
+        pass
+
+    def rose(self, x, y, r, leafcnt):
+        pass
+
+    def koch_snowflake(self):
+        pass
+
+# canvas.line((0, 0) + img.size, fill=255)
