231c6610866a14d322d13236aba4470d05b9f09d
Miroslav Hájek <mirkousko@gmail.com>
2017-07-04 20:15:45 +0200

    Rose drawing with full configurability

--- avatarlogo.py
+++ avatarlogo.py
@@ -7,14 +7,23 @@ class MathLogo:
         self.img = Image.new('RGB', size)
         self.canvas = ImageDraw.Draw(img)
 
+
     def save(self, filename):
         self.img.save(filename, filename.rpartition('.').upper())
-        
+
+
     def __rose_calc(self, degrees):
         pass
 
-    def rose(self, x, y, r, leafcnt):
-        pass
+
+    def rose(self, x, y, r, leafcnt, rgb=(255, 255, 255)):
+        prevx, prevy = self.__rose_calc(0)
+        
+        for angle in range(360):
+            x, y = self.__rose_calc(angle)
+            self.canvas.line((prevx, prevy), (x, y), fill=rgb)
+            prevx, prevy = x, y
+
 
     def koch_snowflake(self):
         pass
