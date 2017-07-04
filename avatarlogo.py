4476f2db615a03b388c9f49b735b4eaa9e06dc54
Miroslav Hájek <mirkousko@gmail.com>
2017-07-04 21:44:40 +0200

    White space clean up

--- avatarlogo.py
+++ avatarlogo.py
@@ -1,6 +1,7 @@
 import math
 from PIL import Image, ImageDraw
 
+
 class MathLogo:
 
     def __init__(self, img):
@@ -8,13 +9,12 @@ class MathLogo:
         self.img = img
         self.canvas = ImageDraw.Draw(img)
 
-
     def save(self, filename):
         self.img.save(filename, filename.rpartition('.')[-1].upper())
 
 
 class Rose(MathLogo):
-    
+
     def __init__(self, img, x, y, r, leafcnt, color=(255, 255, 255)):
         super().__init__(img)
         self.x = x
@@ -23,15 +23,12 @@ class Rose(MathLogo):
         self.leaves = leafcnt
         self.rgb = color
 
-
     def __rose_calc(self, degrees):
         rad = math.radians(degrees)
-        faktor = math.cos(self.leaves * rad)
-        
+        faktor = math.cos(self.leaves * rad)        
         x = self.r * faktor * math.cos(rad) + self.x
         y = self.r * faktor * math.sin(rad) + self.y
         return x, y
-
 
     def rose(self):
         prevx, prevy = self.__rose_calc(0)
@@ -44,8 +41,8 @@ class Rose(MathLogo):
 
 if __name__ == '__main__':
 
-    ico = Image.new('RGB', (640, 480))
-    drawing = Rose(ico, ico.size[0] / 2, ico.size[1] / 2, 100, 4)
+    ico = Image.new('RGB', (500, 500))
+    drawing = Rose(ico, ico.size[0] / 2, ico.size[1] / 2, 200, 4)
     drawing.rose()
     drawing.save('icon.png')
 
