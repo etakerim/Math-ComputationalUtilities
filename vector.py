309a3742678450db7784d2991352251886c78b42
Miroslav Hájek <mirkousko@gmail.com>
2017-07-05 19:20:29 +0200

    Basic vector funcionality

---
+++ vector.py
@@ -0,0 +1,17 @@
+import math
+
+class Vector2D:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector2D(x, y)
+
+    def __sub__(self, other):
+        x = self.x - other.x
+        y = self.y - other.y
+        return Vector2D(x, y)
