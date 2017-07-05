07363d0ec4f9ba5a1c81d2430a89f519dfd4725b
Miroslav Hájek <mirkousko@gmail.com>
2017-07-05 20:28:34 +0200

    Rotation logic

--- vector.py
+++ vector.py
@@ -31,15 +31,20 @@ class Vector2D:
         return self.__mul__(other)
 
     def __repr__(self):
-        return ("Vector2D (x={}, y={})".format(self.x, self.y))
+        return ("Vector2D(x={:.2f}, y={:.2f})".format(self.x, self.y))
 
     def __abs__(self):
         return math.hypot(self.x, self.y)
 
     def angle(self, other):
         return math.acos(self.__mul__(other) 
-                / (self.__abs__() * other.__abs__()))
+                         / (self.__abs__() * other.__abs__()))
 
+    def rotate(self, angle):
+        x = self.x * math.cos(angle) - self.y * math.sin(angle)
+        y = self.x * math.sin(angle) + self.y * math.cos(angle)
+        return Vector2D(x, y)
+        
     def is_perpendicular(self, other):
         return self.__mul__(other) == 0
 
@@ -53,3 +58,4 @@ if __name__ == '__main__':
     print(abs(A))
     print(Vector2D(1, 6).angle(A))
     print(Vector2D(0, 5).is_perpendicular(Vector2D(5, 0)))
+    print(Vector2D(0, 5).rotate(-math.pi))
