dfb94374a406a7d5a5b7ba8efac57bddbe1cfdb5
Miroslav Hájek <mirkousko@gmail.com>
2017-07-15 11:18:37 +0200

    Refactor to logical blocks of creation

--- math_shapener.py
+++ math_shapener.py
@@ -17,45 +17,61 @@ class Canvas(QWidget):
 
 
 class MathShapener(QWidget):
+    MSHAPES = ['Sínus', 'Kosínus', 'Lissajousova krivka', 
+                'Vektor', 'Kruh', 'Ruža', 'Kochova krivka', 
+                'Kochova vločka', 'Serpinského koberec', 
+                'Fraktálový strom', 'Mandelbrotova množina', 
+                'L-system (Korytnačka)']
+
     def __init__(self):
         super().__init__()
         self.mainlayout = QHBoxLayout()
 
         self.leftlayout = QVBoxLayout()
-        self.topmenu = QHBoxLayout()
-        self.shapesel = QComboBox()
-        mshapes = ['Sínus', 'Kosínus', 'Lissajousova krivka', 
-                   'Vektor', 'Kruh', 'Ruža', 'Kochova krivka', 
-                   'Kochova vločka', 'Serpinského koberec', 
-                   'Fraktálový strom', 'Mandelbrotova množina', 
-                   'L-system (Korytnačka)']
-        self.shapesel.addItems(mshapes)
-        self.isgridactive = QCheckBox('Mriežka')
-        self.topmenu.addWidget(self.shapesel, 4)
-        self.topmenu.addWidget(self.isgridactive, 2)
-
+        self.leftlayout.addLayout(self.shape_select())
         self.canvas = Canvas()
-        self.leftlayout.addLayout(self.topmenu)
         self.leftlayout.addWidget(self.canvas)
-
+        
         self.rightlayout = QVBoxLayout()
-        self.coordgroup = QGroupBox('Súradnice')
+        self.rightlayout.addWidget(self.xy_setting())
+        
+        self.mainlayout.addLayout(self.leftlayout, 4)
+        self.mainlayout.addLayout(self.rightlayout, 2)
+        self.setLayout(self.mainlayout)
+
+    def xy_setting(self):
+        coor = QGroupBox('Súradnice')
         self.posinfo_layout = QFormLayout()
+        
         self.xlabel = QLabel('X: ')
         self.xdata = QSpinBox()
         self.xdata.setRange(0, 600)
         self.ylabel = QLabel('Y: ')
         self.ydata = QSpinBox()
         self.ydata.setRange(0, 600)
+
         self.posinfo_layout.addRow(self.xlabel, self.xdata)
         self.posinfo_layout.addRow(self.ylabel, self.ydata)
-        self.coordgroup.setLayout(self.posinfo_layout)
-        self.rightlayout.addWidget(self.coordgroup)
-        
-        self.mainlayout.addLayout(self.leftlayout, 4)
-        self.mainlayout.addLayout(self.rightlayout, 2)
-        self.setLayout(self.mainlayout)
+        coor.setLayout(self.posinfo_layout)
+        return coor
 
+    def shape_select(self):
+        topmenu = QHBoxLayout()
+        
+        self.shapesel = QComboBox()
+        self.shapesel.addItems(self.MSHAPES)
+        self.isgridactive = QCheckBox('Mriežka')
+        self.savebtn = QPushButton('Uložiť')
+        
+        topmenu.addWidget(self.shapesel, 5)
+        topmenu.addWidget(self.savebtn, 2)
+        topmenu.addWidget(self.isgridactive, 2)
+        return topmenu
+
+
+    def actions_global(self):
+        # self.shapesel.activated('') = #func add widgets + their signals
+        pass
 
 class AppWindow(QMainWindow):
     def __init__(self, title, min_size):
